629de0efbc3bcbec2c2648e1fbceceedb47803ac
Benni <benni@bennis-mbp.speedport.ip>
2025-10-19 13:05:18 +0200

    Quartz sync: Oct 19, 2025, 1:05 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,7 +24,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "JetBrains Mono",
-        body: "Lexend",
+        body: "Victor Mono",
         code: "IBM Plex Mono",
       },
       colors: {
