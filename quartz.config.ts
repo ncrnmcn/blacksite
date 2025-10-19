ac444b5c45934f5003720890de53499ce5c041dc
Benni <benni@bennis-mbp.speedport.ip>
2025-10-19 15:33:36 +0200

    Quartz sync: Oct 19, 2025, 3:33 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -40,11 +40,11 @@ const config: QuartzConfig = {
           textHighlight: "#fff23688",
         },
         darkMode: {
-          light: "#161618", //background
-          lightgray: "#006e42", // searchbox and horizontal line
+          light: "#161618",
+          lightgray: "#393639",
           gray: "#646464",
-          darkgray: "#006e42",
-          dark: "#006e42",
+          darkgray: "#d4d4d4",
+          dark: "#ebebec",
           secondary: "#7b97aa",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
